aaa49f9215f6ca5a83c9a49c0d2f64e96b141f39
Mohammed Almubarak <support1@posbank.me>
2024-03-25 15:23:09 +0300

    ..

--- generator/lib/src/code_chunks.dart
+++ generator/lib/src/code_chunks.dart
@@ -256,9 +256,8 @@ class CodeChunks {
         return 'Float32List(0)';
       case 'Float64List':
         return 'Float64List(0)';
-      default: //TODO:check
-        return "''";
-      //   throw InvalidGenerationSourceError('Cannot figure out default value for field: ${p.fieldType} ${p.name}');
+      default:
+        throw InvalidGenerationSourceError('Cannot figure out default value for field: ${p.fieldType} ${p.name}');
     }
   }
 
@@ -491,6 +490,7 @@ class CodeChunks {
           // still makes sense to keep `asciiOptimization: true`
           // `readAll` faster(6.1ms) than when false(8.1ms) on Flutter 3.0.1, Dart 2.17.1
           if (p.isEnum) {
+            //TODO: MUST bring default value
             return '${p.enumName}.values.firstWhere((x) => x.name == ${readFieldCodeString(p, 'fb.StringReader(asciiOptimization: true)', defaultValue: 'to be put')}';
           } else {
             return readFieldCodeString(p, 'fb.StringReader(asciiOptimization: true)');
